51182d68e722026baa5b3a6ada71066d6bab9259
MagicDog <creature.secret@gmail.com>
2023-04-21 17:24:18 -0400

    update basics tests

--- tarea 1/p1-test.rkt
+++ tarea 1/p1-test.rkt
@@ -7,39 +7,39 @@
 ;; recibe solo bool
 (test (run '{#t}) (boolV #t))
 ;; recibe solo cons
-(test (run '{cons 1 2}) (pairV (numV 1) (numV 2)))
+(test (run '{{cons 1 2}}) (pairV (numV 1) (numV 2)))
 ;; vemos si aplica add1
-(test (run '{add1 3}) (numV 4))
+(test (run '{{add1 3}}) (numV 4))
 ;; vemos si aplica +
-(test (run '{+ 3 4}) (numV 7))
+(test (run '{{+ 3 4}}) (numV 7))
 ;; vemos si aplica <
-(test (run '{< 3 4}) (boolV #t))
-(test (run '{< 4 3}) (boolV #f))
+(test (run '{{< 3 4}}) (boolV #t))
+(test (run '{{< 4 3}}) (boolV #f))
 ;; vemos si aplica =
-(test (run '{= 1 1}) (boolV #t))
-(test (run '{= 2 3}) (boolV #f))
+(test (run '{{= 1 1}}) (boolV #t))
+(test (run '{{= 2 3}}) (boolV #f))
 ;; vemos si aplica !
-(test (run '{! #t}) (boolV #f))
-(test (run '{! #f}) (boolV #t))
+(test (run '{{! #t}}) (boolV #f))
+(test (run '{{! #f}}) (boolV #t))
 ;; vemos si aplica &&
-(test (run '{&& #t #t}) (boolV #t))
-(test (run '{&& #f #t}) (boolV #f))
-(test (run '{&& #t #f}) (boolV #f))
-(test (run '{&& #f #f}) (boolV #f))
+(test (run '{{&& #t #t}}) (boolV #t))
+(test (run '{{&& #f #t}}) (boolV #f))
+(test (run '{{&& #t #f}}) (boolV #f))
+(test (run '{{&& #f #f}}) (boolV #f))
 ;; vemos si aplica ||
-(test (run '{|| #t #t}) (boolV #t))
-(test (run '{|| #f #t}) (boolV #t))
-(test (run '{|| #t #f}) (boolV #t))
-(test (run '{|| #f #f}) (boolV #f))
+(test (run '{{|| #t #t}}) (boolV #t))
+(test (run '{{|| #f #t}}) (boolV #t))
+(test (run '{{|| #t #f}}) (boolV #t))
+(test (run '{{|| #f #f}}) (boolV #f))
 ;; vemos si aplica fst
-(test (run '{fst {cons 1 2}}) (numV 1))
+(test (run '{{fst {cons 1 2}}}) (numV 1))
 ;; vemos si aplica snd
-(test (run '{snd {cons 3 4}}) (numV 4))
+(test (run '{{snd {cons 3 4}}}) (numV 4))
 ;; vemos si aplica if
-(test (run '{if #t {+ 1 2} {+ 3 4}}) (numV 3))
-(test (run '{if #f {+ 1 2} {+ 3 4}}) (numV 7))
+(test (run '{{if #t {+ 1 2} {+ 3 4}}}) (numV 3))
+(test (run '{{if #f {+ 1 2} {+ 3 4}}}) (numV 7))
 ;; vemos si aplica with
-(test (run '{with {x 1} {add1 x}}) (numV 2))
+(test (run '{{with {x 1} {add1 x}}}) (numV 2))
 ;; vemos si aplica una funcion
 (test (run '{{define {add2 x} {+ 2 x}}
              {add2 4}}) (numV 6))
