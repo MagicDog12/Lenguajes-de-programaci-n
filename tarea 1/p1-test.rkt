5bbb8612a61bea95886e14596bb2776df371e357
MagicDog12 <creature.secret@gmail.com>
2023-04-22 18:47:02 -0400

    fix comments in tests

--- tarea 1/p1-test.rkt
+++ tarea 1/p1-test.rkt
@@ -54,29 +54,29 @@
 ;; Programa de Ejemplo con más de un argumento en el define
 (test (run '{{define {add3 x y z} {+ {+ x y} z}}
              {add3 2 3 4}}) (numV 9))
-
-(test (run '{ ;; Programa de Ejemplo 1
+;; Programa de Ejemplo 1
+(test (run '{ 
              {define {sum x y z} {+ x {+ y z}}}
              {define {cadr x} {fst {snd x}}}
              {with {{x 9} {y {cons 1 {cons 3 4}}}}
                    {sum x {fst y} {cadr y}} }
              })
       (numV 13))
-
-(test (run '{ ;; Programa de Ejemplo 2
+;; Programa de Ejemplo 2
+(test (run '{ 
              {with {{x 5} {y 23} {z {cons 11 -3}}}
                    z}
              })
       (pairV (numV 11) (numV -3)))
-
-(test (run '{ ;; Programa de Ejemplo 3
+;; Programa de Ejemplo 3
+(test (run '{ 
              {define {triple x} {+ x {+ x x}}}
              {define {add2 x} {+ 2 x}}
              {add2 {triple 2}}
              })
       (numV 8))
-
-(test (run '{ ;; Programa de Ejemplo 4
+;; Programa de Ejemplo 4
+(test (run '{ 
              {with {{x 3} {y {+ 1 2}}}
                    {if {= x y} x y}}
              })
